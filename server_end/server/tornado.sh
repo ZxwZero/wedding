89eb4fdca31ae8abd88f9efa15ab0cada8677a97
xiangwei.zheng <xiangwei.zheng@wenba100.com>
2021-08-31 16:02:07 +0800

    tornado.sh

--- server_end/server/tornado.sh
+++ server_end/server/tornado.sh
@@ -2,6 +2,6 @@
 
 
 run_tornado() {
-    /bin/python3  /home/work/bin/server/main.py -port=8500 >> /home/work/log/tornado0.log 2>&1 &
+    python3  /data/app/wedding/server_end/server/main.py -port=80 >> /data/logs/tornado0.log 2>&1 &
     }
 run_tornado
